ae41374d77771b346ebaa12edd181f3b53a43eeb
meet <dodiyameet041@gmail.com>
2023-10-06 20:45:17 +0530

    Video Call Added

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Appointment from './pages/Dashboard/Patient/Appointment';
 import Doctor from './pages/Doctor';
 import Admin from './pages/Admin';
 import Vadmin from './pages/Vadmin';
+import Video from './pages/Video';
 import Navbar from './Components/Navbar/Navbar';
 import { getLoggedinUser } from './redux/Auth/authActions';
 import { useEffect } from 'react';
@@ -31,11 +32,9 @@ function App() {
   return (
     <Router>
       <Routes>
+      <Route path='/' element={<Home />} />
         <Route element={<PrivateRoutes />}>
-          <Route path='/' element={<Home />} />
-
           <Route path='/contact' element={<ContactUs />} />
-
           <Route path='/doctorregister' element={<DoctorRegister />} />
           <Route path='/patientregister' element={<PatientRegister />} />
           <Route path='/vendorregister' element={<VendorRegister />} />
@@ -43,6 +42,7 @@ function App() {
           <Route path='/patient/order' element={<UserOrder />} />
           <Route path='/patient/appointment' element={<Appointment />} />
           <Route path='/doctor' element={<Doctor />} />
+          <Route path='/video' element={<Video />} />
         </Route>
 
         <Route path='/doctorregister' element={<DoctorRegister />} />
