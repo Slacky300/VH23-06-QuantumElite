d50f04a8e114cc7ed9095c931434102a53618414
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-10-06 20:26:27 +0530

    protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -16,34 +16,44 @@ import Register from './pages/Auth/DoctorRegister';
 import Admin from './pages/Admin';
 import Vadmin from './pages/Vadmin';
 import Navbar from './Components/Navbar/Navbar';
-
+import { getLoggedinUser } from './redux/Auth/authActions';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import PrivateRoutes from './utils/PrivateRoutes';
 
 function App() {
+  const dispatch = useDispatch()
 
+  useEffect(() => {
+    dispatch(getLoggedinUser())
+  }, [])
 
 
   return (
     <Router>
 
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/*' element={<NotFound />} />
-        <Route path='/contact' element={<ContactUs />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/doctorregister' element={<DoctorRegister />} />
-        <Route path='/patientregister' element={<PatientRegister />} />
-        <Route path='/vendorregister' element={<VendorRegister />} />
-        <Route path='/patient' element={<Pdash />} />
-        <Route path='/patient/order' element={<UserOrder />} />
-        <Route path='/patient/appointment' element={<Appointment />} />
-        <Route path='/doctor' element={<Doctor />} />
-        <Route path='/doctorregister' element={<DoctorRegister />} />
-        <Route path='/patientregister' element={<PatientRegister />} />
-        <Route path='/vendorregister' element={<VendorRegister />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path='/' element={<Home />} />
 
+          <Route path='/contact' element={<ContactUs />} />
+
+          <Route path='/doctorregister' element={<DoctorRegister />} />
+          <Route path='/patientregister' element={<PatientRegister />} />
+          <Route path='/vendorregister' element={<VendorRegister />} />
+          <Route path='/patient' element={<Pdash />} />
+          <Route path='/patient/order' element={<UserOrder />} />
+          <Route path='/patient/appointment' element={<Appointment />} />
+          <Route path='/doctor' element={<Doctor />} />
+        </Route>
+
+        <Route path='/doctorregister' element={<DoctorRegister />} />
+        <Route path='/patientregister' element={<PatientRegister />} />
+        <Route path='/vendorregister' element={<VendorRegister />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/admin' element={<Admin />} />
         <Route path='/vadmin' element={<Vadmin />} />
-
+        <Route path='/*' element={<NotFound />} />
 
 
       </Routes>
--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,100 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BiMenuAltRight } from 'react-icons/bi'
+import logo from '../../images/logo.png'
+import { Link } from "react-router-dom"
+import '../../styles/navbar.css'
 
 const Navbar = () => {
-  return(
+
+  const [color, setcolor] = useState(false)
+
+  const changeColor = () => {
+    if (window.scrollY >= 90) {
+      setcolor(true)
+    } else {
+      setcolor(false)
+    }
+  }
+
+  window.addEventListener('scroll', changeColor)
+
+
+  useEffect(() => {
+    const navBar = document.querySelectorAll(".nav-link");
+    const navCollapse = document.querySelector(".navbar-collapse.collapse");
+
+    const handleNavClick = () => {
+      navCollapse.classList.remove("show");
+    };
+
+    navBar.forEach((a) => {
+      a.addEventListener("click", handleNavClick);
+    });
+
+    return () => {
+      navBar.forEach((a) => {
+        a.removeEventListener("click", handleNavClick);
+      });
+    };
+  }, []);
+
+  return (
     <>
-    Navbar
+      <>
+        <header className={color ? 'header_wrapper header-scrolled' : 'header_wrapper'}>
+          <nav className="navbar navbar-expand-lg fixed-top">
+            <div className="container-fluid mx-3">
+              <Link to='/'>
+                <img src={logo} style={{ width: '130px' }} />
+              </Link>
+              <button className="navbar-toggler pe-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <BiMenuAltRight size={35} />
+              </button>
+              <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul className="navbar-nav menu-navbar-nav">
+                  <Link to='/' style={{ textDecoration: 'none' }}>
+                    <li className="nav-item">
+                      <a className="nav-link " aria-current="page">Home</a>
+                    </li>
+                  </Link>
+                  <Link to='/about' style={{ textDecoration: 'none' }}>
+                    <li className="nav-item">
+                      <a className="nav-link " aria-current="page">Medicines</a>
+                    </li>
+                  </Link>
+                  <Link to='/contact' style={{ textDecoration: 'none' }}>
+                    <li className="nav-item">
+                      <a className="nav-link " aria-current="page">Doctor</a>
+                    </li>
+                  </Link>
+                </ul>
+
+                <ul className='mt-2 text-center'>
+                  <Link to='/login' style={{ textDecoration: 'none' }} className="nav-item text-center">
+                    <a className="nav-link learn-more-btn btn-extra-header" aria-current="page">Login</a>
+                  </Link>
+                  <Link to='/patientregister' style={{ textDecoration: 'none' }} className="nav-item text-center">
+                    <a className="nav-link learn-more-btn" aria-current="page">Register</a>
+                  </Link>
+                </ul>
+
+                {/* <ul className='mt-2 text-center'>
+                                    <Link style={{ textDecoration: 'none' }} className="nav-item text-center">
+                                        <a className="nav-link learn-more-btn" aria-current="page">Dashboard</a>
+                                    </Link>
+                                    <Link to='/login' style={{ textDecoration: 'none' }} className="nav-item text-center">
+                                        <a className="nav-link learn-more-btn-logout" aria-current="page">Logout</a>
+                                    </Link>
+                                </ul> */}
+
+              </div>
+            </div>
+          </nav>
+        </header>
+      </>
+
     </>
-    )
+  )
 }
 
 export default Navbar
