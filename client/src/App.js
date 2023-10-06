9c83c2814a9437a6a8b665c6011803089e0d4407
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-10-06 19:40:01 +0530

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast';
 import './App.css';
 import NotFound from './Components/Errors/404';
@@ -20,41 +20,45 @@ import Register from './pages/Auth/DoctorRegister';
 import Admin from './pages/Admin';
 import Vadmin from './pages/Vadmin';
 
+
 function App() {
+
+
+
   return (
     <Router>
-      <Routes> 
-      <Route path='/' element={<Home />} />
-      <Route path='/*' element={<NotFound/>} />
-      <Route path='/contact' element={<ContactUs/>} />
-      <Route path='/login' element={<Login/>} />
-      <Route path='/doctorregister' element={<DoctorRegister/>} />
-      <Route path='/patientregister' element={<PatientRegister/>} />
-      <Route path='/vendorregister' element={<VendorRegister/>} />
-      <Route path='/emergency' element={<Emergency/>} />
-      <Route path='/incident' element={<Incident/>} />
-      <Route path='/closedreport' element={<CloseFile/>} />
-      <Route path='/chat' element={<ChatScreen/>} />
-      <Route path='/patient' element={<Pdash/>} />
-      <Route path='/patient/order' element={<UserOrder/>} />
-      <Route path='/patient/appointment' element={<Appointment/>} />
-      <Route path='/doctor' element={<Doctor/>} />
-      <Route path='/doctorregister' element={<DoctorRegister/>} />
-      <Route path='/patientregister' element={<PatientRegister/>} />
-      <Route path='/vendorregister' element={<VendorRegister/>} />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/*' element={<NotFound />} />
+        <Route path='/contact' element={<ContactUs />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/doctorregister' element={<DoctorRegister />} />
+        <Route path='/patientregister' element={<PatientRegister />} />
+        <Route path='/vendorregister' element={<VendorRegister />} />
+        <Route path='/emergency' element={<Emergency />} />
+        <Route path='/incident' element={<Incident />} />
+        <Route path='/closedreport' element={<CloseFile />} />
+        <Route path='/chat' element={<ChatScreen />} />
+        <Route path='/patient' element={<Pdash />} />
+        <Route path='/patient/order' element={<UserOrder />} />
+        <Route path='/patient/appointment' element={<Appointment />} />
+        <Route path='/doctor' element={<Doctor />} />
+        <Route path='/doctorregister' element={<DoctorRegister />} />
+        <Route path='/patientregister' element={<PatientRegister />} />
+        <Route path='/vendorregister' element={<VendorRegister />} />
 
-      <Route path='/admin' element={<Admin/>} />
-      <Route path='/vadmin' element={<Vadmin/>} />
-      
+        <Route path='/admin' element={<Admin />} />
+        <Route path='/vadmin' element={<Vadmin />} />
+
 
 
       </Routes>
       <Toaster />
     </Router>
-  
+
   )
-  ;
-  
+    ;
+
 }
 
 export default App;
