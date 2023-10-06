6dc4ea0c77b76efe0535ec7b9394cac8d7ff05a5
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-10-06 22:17:15 +0530

    get doctors

--- server/routes/doctor.routes.js
+++ server/routes/doctor.routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {getAllDoctors,getPatients, editDoctorDetails} = require('../controllers/doctor.controllers')
-const {verifyJWT} = require("../middlewares/verifyJWT");
+const { getAllDoctors, getPatients, editDoctorDetails, getDoc } = require('../controllers/doctor.controllers')
+const { verifyJWT } = require("../middlewares/verifyJWT");
 
 
 router.route('/all-doctors').get(verifyJWT, getAllDoctors);
 router.route('/:doctorId').get(verifyJWT, getPatients).put(verifyJWT, editDoctorDetails);
+router.route('/doctor-details/:id').get(verifyJWT,)
 
 module.exports = router;
