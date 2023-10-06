6c999c0186ff82b896a3b210e20799581c3ee4d8
Aditya <192105adityashah@gmail.com>
2023-10-06 19:37:04 +0530

    feature/updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import './App.css';
 import NotFound from './Components/Errors/404';
-import Home from './pages/Home'
+import Home from './pages/Home';
 import Login from './pages/Auth/Login';
 import DoctorRegister from './pages/Auth/DoctorRegister';
 import ContactUs from './Components/ContactUs';
@@ -16,40 +16,36 @@ import Register from './pages/Auth/DoctorRegister';
 import Admin from './pages/Admin';
 import Vadmin from './pages/Vadmin';
 import Navbar from './Components/Navbar/Navbar';
+import Vdash from './pages/VenderDash/Vdash';
+import { useState } from 'react';
 
 function App() {
+
   return (
     <Router>
-      <Navbar/>
-      <Routes> 
-        
-      <Route path='/' element={<Home />} />
-      <Route path='/*' element={<NotFound/>} />
-      <Route path='/contact' element={<ContactUs/>} />
-      <Route path='/login' element={<Login/>} />
-      <Route path='/doctorregister' element={<DoctorRegister/>} />
-      <Route path='/patientregister' element={<PatientRegister/>} />
-      <Route path='/vendorregister' element={<VendorRegister/>} />
-      <Route path='/patient' element={<Pdash/>} />
-      <Route path='/patient/order' element={<UserOrder/>} />
-      <Route path='/patient/appointment' element={<Appointment/>} />
-      <Route path='/doctor' element={<Doctor/>} />
-      <Route path='/doctorregister' element={<DoctorRegister/>} />
-      <Route path='/patientregister' element={<PatientRegister/>} />
-      <Route path='/vendorregister' element={<VendorRegister/>} />
-
-      <Route path='/admin' element={<Admin/>} />
-      <Route path='/vadmin' element={<Vadmin/>} />
-      
-
-
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/*' element={<NotFound />} />
+        <Route path='/contact' element={<ContactUs />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/doctorregister' element={<DoctorRegister />} />
+        <Route path='/patientregister' element={<PatientRegister />} />
+        <Route path='/vendorregister' element={<VendorRegister />} />
+        <Route path='/patient' element={<Pdash />} />
+        <Route path='/patient/order' element={<UserOrder />} />
+        <Route path='/patient/appointment' element={<Appointment />} />
+        <Route path='/doctor' element={<Doctor />} />
+        <Route path='/doctorregister' element={<DoctorRegister />} />
+        <Route path='/patientregister' element={<PatientRegister />} />
+        <Route path='/vendorregister' element={<VendorRegister />} />
+        <Route path='/vdash' element={<Vdash />} />
+        <Route path='/admin' element={<Admin />} />
+        <Route path='/vadmin' element={<Vadmin />} />
       </Routes>
       <Toaster />
     </Router>
-  
-  )
-  ;
-  
+  );
 }
 
 export default App;
---
+++ client/src/pages/VenderDash/Vdash.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+
+const Vdash = () => {
+  return (
+    <div >
+      <div className="row">
+        <div className="col-md-3">
+          <Sidebar/>
+        </div>
+        <div className="col-md-9">
+          <h1>fdfds</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Vdash
