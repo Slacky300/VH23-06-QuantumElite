92c4ce9d77ba93ec30f8749ac13dcf92338b2abb
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-10-06 12:15:15 +0530

    auth process

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const { registerUser, loginUser } = require('../controllers/auth.controllers')
+const { upload } = require('../middlewares/upload.middleware')
 
-router.post('/register', registerUser)
+router.post('/register', upload.single('certification'), registerUser)
 router.post('/login', loginUser)
 
 module.exports = router
