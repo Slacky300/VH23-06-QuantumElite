3225a592b86c5258998e99eb94befda7d1fb7636
Slacky300 <rehman.khan.300a@gmail.com>
2023-10-06 22:16:44 +0530

    /bugfix/getdoctordetails

--- server/routes/doctor.routes.js
+++ server/routes/doctor.routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {getAllDoctors,getPatients, editDoctorDetails} = require('../controllers/doctor.controllers')
+const {getAllDoctors,getPatients, editDoctorDetails,getDoctorDetails} = require('../controllers/doctor.controllers')
 const {verifyJWT} = require("../middlewares/verifyJWT");
 
 
 router.route('/all-doctors').get(verifyJWT, getAllDoctors);
 router.route('/:doctorId').get(verifyJWT, getPatients).put(verifyJWT, editDoctorDetails);
+router.route('/doctor-details/:id').get(verifyJWT, getDoctorDetails)
 
 module.exports = router;
