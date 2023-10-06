6d4a595965c655194bc1353ae938ee8d49460fd0
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-10-07 04:05:00 +0530

    patient and user modal

--- client/src/pages/VenderDash/Vdash.jsx
+++ client/src/pages/VenderDash/Vdash.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import Stats from "../../Components/Dash/Stats"
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const Vdash = () => {
+  const navigate = useNavigate();
+  const user = useSelector((state) => state?.auth?.user)
+
+  if (user?.role !== "vendor") {
+    navigate("/");
+  }
+
   return (
     <div className="container marginStyle">
       <div className="container-fluid">
